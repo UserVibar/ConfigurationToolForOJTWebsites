02e82a8b715c6d69f4f070913ceec842ccdcafdb
unknown <vibar.130437@marikina.sti.edu.ph>
2024-05-22 17:35:56 +0800

    first commit

---
+++ ConfigurationTool/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace ConfigurationTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start_Server = new System.Windows.Forms.Button();
+            this.PRSSystemFolderPath = new System.Windows.Forms.TextBox();
+            this.Choose_Folder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Host = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.DBName = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.IconButton = new System.Windows.Forms.Button();
+            this.Icon = new System.Windows.Forms.PictureBox();
+            this.CompanyName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Icon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start_Server
+            // 
+            this.Start_Server.Location = new System.Drawing.Point(12, 440);
+            this.Start_Server.Name = "Start_Server";
+            this.Start_Server.Size = new System.Drawing.Size(327, 23);
+            this.Start_Server.TabIndex = 0;
+            this.Start_Server.Text = "Start Server";
+            this.Start_Server.UseVisualStyleBackColor = true;
+            this.Start_Server.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PRSSystemFolderPath
+            // 
+            this.PRSSystemFolderPath.Enabled = false;
+            this.PRSSystemFolderPath.Location = new System.Drawing.Point(15, 25);
+            this.PRSSystemFolderPath.Name = "PRSSystemFolderPath";
+            this.PRSSystemFolderPath.Size = new System.Drawing.Size(243, 20);
+            this.PRSSystemFolderPath.TabIndex = 1;
+            // 
+            // Choose_Folder
+            // 
+            this.Choose_Folder.Location = new System.Drawing.Point(264, 23);
+            this.Choose_Folder.Name = "Choose_Folder";
+            this.Choose_Folder.Size = new System.Drawing.Size(75, 23);
+            this.Choose_Folder.TabIndex = 2;
+            this.Choose_Folder.Text = "Browse";
+            this.Choose_Folder.UseVisualStyleBackColor = true;
+            this.Choose_Folder.Click += new System.EventHandler(this.Choose_Folder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PRS System Folder Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Database Host:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Database Port:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Database Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Username:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Password:";
+            // 
+            // Host
+            // 
+            this.Host.Enabled = false;
+            this.Host.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Host.Location = new System.Drawing.Point(113, 19);
+            this.Host.Name = "Host";
+            this.Host.Size = new System.Drawing.Size(208, 20);
+            this.Host.TabIndex = 1;
+            // 
+            // Port
+            // 
+            this.Port.Enabled = false;
+            this.Port.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Port.Location = new System.Drawing.Point(113, 54);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(208, 20);
+            this.Port.TabIndex = 5;
+            // 
+            // DBName
+            // 
+            this.DBName.Enabled = false;
+            this.DBName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DBName.Location = new System.Drawing.Point(113, 89);
+            this.DBName.Name = "DBName";
+            this.DBName.Size = new System.Drawing.Size(208, 20);
+            this.DBName.TabIndex = 5;
+            // 
+            // Username
+            // 
+            this.Username.Enabled = false;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(113, 124);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(208, 20);
+            this.Username.TabIndex = 6;
+            // 
+            // Password
+            // 
+            this.Password.Enabled = false;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(113, 159);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(208, 20);
+            this.Password.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Host);
+            this.groupBox1.Controls.Add(this.Password);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Username);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.DBName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.Port);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 246);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 188);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PRS System Database";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.CompanyName);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 188);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Company Details";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.IconButton);
+            this.groupBox4.Controls.Add(this.Icon);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(6, 45);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(315, 132);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Icon:";
+            // 
+            // IconButton
+            // 
+            this.IconButton.Enabled = false;
+            this.IconButton.Location = new System.Drawing.Point(6, 100);
+            this.IconButton.Name = "IconButton";
+            this.IconButton.Size = new System.Drawing.Size(300, 23);
+            this.IconButton.TabIndex = 10;
+            this.IconButton.Text = "Change Icon";
+            this.IconButton.UseVisualStyleBackColor = true;
+            this.IconButton.Click += new System.EventHandler(this.IconButton_Click);
+            // 
+            // Icon
+            // 
+            this.Icon.Location = new System.Drawing.Point(6, 19);
+            this.Icon.Name = "Icon";
+            this.Icon.Size = new System.Drawing.Size(300, 75);
+            this.Icon.TabIndex = 9;
+            this.Icon.TabStop = false;
+            // 
+            // CompanyName
+            // 
+            this.CompanyName.Enabled = false;
+            this.CompanyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompanyName.Location = new System.Drawing.Point(113, 19);
+            this.CompanyName.Name = "CompanyName";
+            this.CompanyName.Size = new System.Drawing.Size(208, 20);
+            this.CompanyName.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 472);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Choose_Folder);
+            this.Controls.Add(this.PRSSystemFolderPath);
+            this.Controls.Add(this.Start_Server);
+            this.Name = "Form1";
+            this.Text = "Configuration Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Icon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_Server;
+        private System.Windows.Forms.TextBox PRSSystemFolderPath;
+        private System.Windows.Forms.Button Choose_Folder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Host;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.TextBox DBName;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox CompanyName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox Icon;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button IconButton;
+    }
+}
+
---
+++ ConfigurationTool/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConfigurationTool
+{
+    public partial class Form1 : Form
+    {
+        private string folderPath = Properties.Settings.Default.FolderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string command = "/C cd C:\\Users\\Admin\\Documents\\PRS\\System\\PRS_System & php artisan serve --host 192.168.8.62 --port 8080";
+            System.Diagnostics.Process.Start("CMD.exe", command);
+        }
+
+        private void Choose_Folder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                DialogResult result = dlg.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dlg.SelectedPath))
+                {
+                    this.PRSSystemFolderPath.Text = dlg.SelectedPath;
+                    this.folderPath = dlg.SelectedPath;
+                    Properties.Settings.Default.FolderPath = dlg.SelectedPath;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void IconButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Png Icon|*.png;";
+                DialogResult result = ofd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    this.Icon.Image.Dispose();
+                    File.Copy(ofd.FileName, Path.Combine(this.folderPath + "/public/Icon.png"), true);
+                    this.Icon.Image = Image.FromFile(this.folderPath + "/public/Icon.png");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.PRSSystemFolderPath.Text = Properties.Settings.Default.FolderPath;
+
+            if (!string.IsNullOrEmpty(this.folderPath))
+            {
+                if (File.Exists(this.folderPath + "/.env"))
+                {
+                    this.CompanyName.Enabled = true;
+                    this.IconButton.Enabled = true;
+                    this.Host.Enabled = true;
+                    this.Port.Enabled = true;
+                    this.DBName.Enabled = true;
+                    this.Username.Enabled = true;
+                    this.Password.Enabled = true;
+
+                    if (File.Exists(this.folderPath + "/public/Icon.png"))
+                    {
+                        this.Icon.Image = Image.FromFile(this.folderPath + "/public/Icon.png");
+                        this.Icon.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+                }
+            }
+        }
+    }
+}
